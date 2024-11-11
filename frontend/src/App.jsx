854bee3979b4ea9d5c4aafedfde8d38768ed5aa1
Kir Peñalber <penalberkirstine@gmail.com>
2024-11-11 13:18:55 +0800

    feat: set mobile container in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,10 @@
-import logo from './logo.svg';
+import ParkingLotsPage from "./pages/ParkingLotsPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // Mobile container
+    <div className="max-w-sm bg-white mx-auto h-screen">
+      <ParkingLotsPage />
     </div>
   );
 }
