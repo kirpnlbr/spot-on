c82e9399a997077203c80f5d8d0c26f2b7d05bfe
Kir Peñalber <penalberkirstine@gmail.com>
2024-11-12 16:17:58 +0800

    fix: tweak parkingGrid styles

--- frontend/src/pages/ParkingGridPage.jsx
+++ frontend/src/pages/ParkingGridPage.jsx
@@ -4,9 +4,9 @@ import { ArrowLeftIcon } from "@radix-ui/react-icons";
 
 function ParkingGridPage() {
     return (
-        <div class="flex flex-col bg-white">
+        <div class="flex flex-col space-y-4 bg-white">
             {/* Header */}
-            <div class="flex p-4 items-center">
+            <div class="flex p-4 items-center border-b shadow-sm">
                 <button>
                     <ArrowLeftIcon />
                 </button>
@@ -14,13 +14,15 @@ function ParkingGridPage() {
             </div>
 
             {/* Parking Grid */}
-            <ParkingGrid />
+            <div class="p-4 rounded-md border-[1.5px] border-gray-200"></div>
 
             {/* Navigation Menu */}
-            <div class="flex flex-col p-4 rounded-lg bg-blue-300">
-                <span class="font-medium text-sm text-gray-600">Nearest spot</span>
-                <span class="font-bold text-lg text-gray-800 pb-4">A4</span>
-                <button class="rounded-md px-3 py-2 bg-blue-700 text-white font-medium w-full hover:bg-blue-600">Navigate to Spot</button>
+            <div class="p-4 rounded-lg bg-blue-300">
+                <div class="flex flex-col">
+                    <span class="font-medium text-sm text-gray-600">Nearest spot</span>
+                    <span class="font-bold text-lg text-gray-800 pb-4">A4</span>
+                    <button class="rounded-md px-3 py-2 bg-blue-700 text-white font-medium w-full hover:bg-blue-600">Navigate to Spot</button>
+                </div>
             </div>
 
         </div>
