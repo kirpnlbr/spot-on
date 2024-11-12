07e6882df891cf37616973c5436a909fa83322fb
Kir Peñalber <penalberkirstine@gmail.com>
2024-11-12 15:40:58 +0800

    feat: add `parkingGrid` data structure

--- frontend/src/data/mockData.js
+++ frontend/src/data/mockData.js
@@ -18,3 +18,44 @@ export const parkingLots = [
         spots: "34 spots"
     }
 ];
+
+export const parkingGrid = [
+    {
+        name: "Central Mall Parking",
+        totalSpots: 25,
+        occupiedSpots: 13,
+        spots: [
+            { id: "A1", isOccupied: false },
+            { id: "A2", isOccupied: true },
+            { id: "A3", isOccupied: true },
+            { id: "A4", isOccupied: true },
+            { id: "A5", isOccupied: false },
+            { id: "B1", isOccupied: true },
+            { id: "B2", isOccupied: true },
+            { id: "B3", isOccupied: false },
+            { id: "B4", isOccupied: true },
+            { id: "B5", isOccupied: true },
+            { id: "C1", isOccupied: false },
+            { id: "C2", isOccupied: false },
+            { id: "C3", isOccupied: true },
+            { id: "C4", isOccupied: true },
+            { id: "C5", isOccupied: true },
+            { id: "D1", isOccupied: false },
+            { id: "D2", isOccupied: true },
+            { id: "D3", isOccupied: true },
+            { id: "D4", isOccupied: false },
+            { id: "D5", isOccupied: false },
+            { id: "E1", isOccupied: false },
+            { id: "E2", isOccupied: true },
+            { id: "E3", isOccupied: true },
+            { id: "E4", isOccupied: true },
+            { id: "E5", isOccupied: true },
+            { id: "F1", isOccupied: false },
+            { id: "F2", isOccupied: false },
+            { id: "F3", isOccupied: false },
+            { id: "F4", isOccupied: false },
+            { id: "F5", isOccupied: true },
+        ]
+
+    }
+]
