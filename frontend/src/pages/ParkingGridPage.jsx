c3d1abe3c32d04f5eb7403fab1461bc2507c4050
Kir Peñalber <penalberkirstine@gmail.com>
2024-11-12 23:21:20 +0800

    feat: add parking grid in `parkingGridPage`

--- frontend/src/pages/ParkingGridPage.jsx
+++ frontend/src/pages/ParkingGridPage.jsx
@@ -3,6 +3,9 @@ import ParkingGrid from "../components/parking/ParkingGrid";
 import { ArrowLeftIcon } from "@radix-ui/react-icons";
 
 function ParkingGridPage() {
+
+    const parkingLot = parkingGrid[0];
+
     return (
         <div class="flex flex-col space-y-4 bg-white">
             {/* Header */}
@@ -10,18 +13,20 @@ function ParkingGridPage() {
                 <button>
                     <ArrowLeftIcon />
                 </button>
-                <span class="ml-2 text-lg font-semibold text-gray-900">Central Mall Parking</span>
+                <span class="ml-2 text-lg font-semibold text-gray-900">{parkingLot.name}</span>
             </div>
 
             {/* Parking Grid */}
-            <div class="p-4 rounded-md border-[1.5px] border-gray-200"></div>
+            <div class="p-4 rounded-lg border-[1.5px] border-gray-100">
+                <ParkingGrid />
+            </div>
 
-            {/* Navigation Menu */}
-            <div class="p-4 rounded-lg bg-blue-300">
+            {/* Navigate to Spot */}
+            <div class="p-4 rounded-lg bg-blue-300 border-[1.5px] border-blue-200">
                 <div class="flex flex-col">
                     <span class="font-medium text-sm text-gray-600">Nearest spot</span>
                     <span class="font-bold text-lg text-gray-800 pb-4">A4</span>
-                    <button class="rounded-md px-3 py-2 bg-blue-700 text-white font-medium w-full hover:bg-blue-600">Navigate to Spot</button>
+                    <button class="rounded-lg px-3 py-2 bg-blue-700 border border-blue-600 text-white font-medium w-full hover:bg-blue-600">Navigate to Spot</button>
                 </div>
             </div>
 
