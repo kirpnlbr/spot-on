d04568e39bb2e5a0b82e2036be74a2c04c98571e
Kir Peñalber <penalberkirstine@gmail.com>
2024-11-13 10:25:44 +0800

    feat: create `LocationPage`

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import ParkingLotsPage from "./pages/ParkingLotsPage";
 import ParkingGridPage from "./pages/ParkingGridPage";
 import NavigationPage from "./pages/NavigationPage";
+import LocationPage from "./pages/LocationPage";
 
 function App() {
   return (
     // Mobile container
     <div class="max-w-sm bg-[#fdfdfb] mx-auto h-screen">
+      <LocationPage />
       <ParkingLotsPage />
       <ParkingGridPage />
       <NavigationPage />
--- frontend/src/pages/LocationPage.jsx
+++ frontend/src/pages/LocationPage.jsx
@@ -1,1 +1,19 @@
-s
+import { MapPin } from 'lucide-react';
+
+function LocationPage() {
+    return (
+        <div class="min-h-screen flex flex-col space-y-8 justify-center items-center px-4">
+            <div class="flex flex-col space-y-2 items-center">
+                <MapPin class="text-blue-500 w-10 h-10" />
+                <h1 class="text-2xl font-bold text-gray-900">Enable Location</h1>
+                <p class="text-gray-600 text-center">SpotOn needs your location to find the nearest available parking spot.</p>
+            </div>
+            <div class="flex flex-col space-y-3 items-center">
+                <button class="rounded-xl p-3 bg-blue-700 border-[1.5px] border-blue-500 text-white font-medium w-full hover:bg-blue-600">Allow Location Access</button>
+                <button class="text-blue-700 underline text-sm font-medium w-full ">Maybe Later</button>
+            </div>
+        </div>
+    )
+}
+
+export default LocationPage;
